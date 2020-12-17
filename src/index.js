300d74d6aaf457be664528d79fbb446d8b5af326
He4eT <He4eTHb1u@gmail.com>
2020-12-18 01:13:17 +0500

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
+import {h} from 'preact'
+import {useState} from 'preact/hooks'
+import {Router} from 'preact-router'
+
+import Editor from './routes/editor'
+import Post from './routes/post'
+
 import './style/typography.css'
 import './style'
 
-import App from './app'
+const App = () => {
+  const [markdown, setMarkdown] = useState('')
+
+  return (<main>
+    <Router>
+      <Editor
+        path='/'
+        markdown={ markdown }
+        { ...{setMarkdown} } />
+      <Post
+        path='/preview/'
+        mode='preview'
+        payload={ markdown }
+        { ...{setMarkdown} } />
+      <Post
+        path='/p/:payload'
+        mode='post'
+        { ...{setMarkdown} } />
+    </Router>
+  </main>)
+}
 
 export default App
