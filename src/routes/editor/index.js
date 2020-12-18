7472192326c555a8c0f1785ea3976ceae3290881
He4eT <He4eTHb1u@gmail.com>
2020-12-18 13:47:09 +0500

    Add instructions for the user

--- src/routes/editor/index.js
+++ src/routes/editor/index.js
@@ -5,8 +5,15 @@ import style from './style.css'
 
 const Editor = ({markdown, setMarkdown}) => (
   <section>
+    <h1>Huge link</h1>
+    <p>
+      A&nbsp;minimalist serverless publishing tool
+      that allows you to&nbsp;create markdown posts and
+      push them to&nbsp;the Web by&nbsp;sharing the link.
+    </p>
     <textarea
       class={ style.textarea }
+      placeholder="Write a post using markdown and click Preview"
       onInput={ ({target}) => setMarkdown(target.value) }>
         { markdown }
     </textarea>
